14f6a48e08169a4c7eeecf73ea6f429b583adea7
unknown <sanj.9777@gmail.com>
2018-03-09 00:57:16 +0530

    added new code in csvtojson file

--- csvtojson.js
+++ csvtojson.js
@@ -1,18 +1,44 @@
-const csvtojson = require('csvtojson')
+/* Method 1 */
+
+// const csvtojson = require('csvtojson')
+// const fs = require('fs')
+// const path = require('path')
+// const converter=csvtojson({
+//    flatKeys:true
+// })
+// const csvfilepath = path.join(__dirname,'customer-data.csv')
+// let buff = []
+// csvtojson()
+// .fromFile(csvfilepath)
+// .on('data',(jsonObj)=>{
+//     buff.push(jsonObj)
+//     console.log(buff.length)
+//     fs.writeFileSync(path.join(__dirname, 'customer-data.json'), buff)
+// })
+// .on('done',(error)=>{
+//    fs.appendFileSync(path.join(__dirname, 'customer-data.json'),)
+// })
+
+/* Method 1 */
+
+const csvFilePath = 'customer-data.csv'
 const fs = require('fs')
-const path = require('path')
-const converter=csvtojson({
-   flatKeys:true
-})
-const csvfilepath = path.join(__dirname,'customer-data.csv')
-let buff = []
-csvtojson()
-.fromFile(csvfilepath)
-.on('data',(jsonObj)=>{
-    buff.push(jsonObj)
-    console.log(buff.length)
-    fs.writeFileSync(path.join(__dirname, 'customer-data.json'), buff)
+const csv = require('csvtojson')
+
+let arr = []
+
+csv()
+.fromFile(csvFilePath)
+.on('json',(jsonObj)=>{
+    arr.push(jsonObj)
+    
 })
 .on('done',(error)=>{
-   fs.appendFileSync(path.join(__dirname, 'customer-data.json'),)
+	if(error) return process.exit(1)
+		console.log(arr.length)
+    fs.writeFile('customer-data.json', JSON.stringify(arr,null,2),(error)=>{
+    	if(error) return process.exit(1)
+    		console.log('done')
+    	process.exit(0)
+    })
 })
